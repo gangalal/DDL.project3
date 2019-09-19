8e43b279185f96800c57780cdf7ab112d0c92cc4
D-Glover <55523571+D-Glover@users.noreply.github.com>
2019-09-19 12:38:42 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 /*
- ===============================================================================
- Name        : project2.c
+===============================================================================
+ Name        : Assignment_3.c
  Author      : $(author)
  Version     :
  Copyright   : $(copyright)
  Description : main definition
- ===============================================================================
- */
+===============================================================================
+*/
 
 #ifdef __USE_CMSIS
 #include "LPC17xx.h"
@@ -25,62 +25,78 @@
 #define CLKSRCSEL (*(volatile unsigned int *)(0x400FC10C))
 #define PINSEL3 (*(volatile unsigned int *)(0x4002C00C))
 
-volatile int M = 50;
-volatile int N = 1;
+volatile int M = 51;
+volatile int N = 2;
+
+// PLL0 Feed Function
+void pllFeed (void) {
+	PLL0FEED = 0xAA;
+	PLL0FEED = 0x55;
+}
 
 int main(void) {
-	//CLKSRCSEL |= (0)
-	PLL0CON &= ~(1 << 1);
-	PLL0FEED = 0xAA;
-	PLL0FEED = 0x55;
-	//RC source is default, we didn't write to CLKSRCSEL
-	while ((PLL0STAT & (1 << 24)) == 0x00)
-			;
-	PLL0CON &= ~(1 << 0);
-	PLL0FEED = 0xAA;
-	PLL0FEED = 0x55;
 
-	while ((PLL0STAT & (1 << 24)) == 0x00)
-			;
+	//CLKSRCSEL |= (0);
+//	PLL0CON &= ~(1 << 1);
+//	pllFeed();
+	//RC source is default, we didn't write to CLKSRCSEL
+//	while ((PLL0STAT & (1 << 24)) == 0x00)
+//	;
+	PLL0CON &= ~(1 << 0);
+	pllFeed();
+
+//	while ((PLL0STAT & (1 << 24)) == 0x00)
+//			;
+
+// Choose 4MHz Clock
+	CLKSRCSEL = 0;
+
 	// Turn on PLL0
-	PLL0CFG = 50;//((M-1)<<0)|((N-1)<<16);
-	PLL0FEED = 0xAA; // Feed the PLL
-	PLL0FEED = 0x55;
-	PLL0CON |= 1 << 0;
-	PLL0FEED = 0xAA; // Feed the PLL
-	PLL0FEED = 0x55;
+	PLL0CFG = ((M-1)<<0)|((N-1)<<16);
+	pllFeed();
+	PLL0CON |= (1 << 0);
+	pllFeed();
+
 	// Wait for main PLL (PLL0) to come up
-	while ((PLL0STAT & (1 << 24)) == 0x00)
-		;
+//	while ((PLL0STAT & (1 << 24)) == 0x00)
+//		;
 	// Wait for PLOCK0 to become 1
 	while ((PLL0STAT & (1 << 26)) == 0x00)
-		;
-	// Connect to the PLL0
-	PLL0CON |= 1 << 1;
-	PLL0FEED = 0xAA; // Feed the PLL
-	PLL0FEED = 0x55;
-	while ((PLL0STAT & (1 << 25)) == 0x00)
-		; //Wait for PLL0 to connect
+	{}
+
+	// Enable PLL0 to update
+	PLL0CON = 0x1;
+	pllFeed();
+
+	PLL0CON |= (1<<1);
+	pllFeed();
+//	while ((PLL0STAT & (1 << 25)) == 0x00)
+//		; //Wait for PLL0 to connect
 
 	// shifting 3 into CCLKCFG selecting clock divider value C
-	CCLKCFG |= (1 << 0);
-	CCLKCFG |= (1 << 1);
+	CCLKCFG = 4;
 
-	CLKOUTCFG &= ~(1<<0);
-	CLKOUTCFG &= ~(1<<1);
-	CLKOUTCFG &= ~(1<<2);
-	CLKOUTCFG &= ~(1<<3);
+	// Connect to the PLL0
+//PLL0CON |= 1 << 1;
+//pllFeed();
+
+//CLKOUTCFG &= ~(1<<0);
+//CLKOUTCFG &= ~(1<<1);
+//CLKOUTCFG &= ~(1<<2);
+//CLKOUTCFG &= ~(1<<3);
+
+
 
 
 	// shifting 10 into CLKOUTCFG selecting clock divider value K
-	CLKOUTCFG |= (1 << 5);
-	CLKOUTCFG |= (1 << 7);
+	CLKOUTCFG = 10;
+
+	// enables us to read from P1.27
+	CLKOUTCFG |= (1 << 8);
 
 	// allows us to select P1.27 to measure clock
 	PINSEL3 |= (1 << 22);
 
-	// enables us to read from P1.27
-	CLKOUTCFG |= (1 << 8);
 
 	return 0;
 }
